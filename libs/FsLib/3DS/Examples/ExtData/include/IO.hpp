3adabcab309416340f6c4a7e25a54bab0abaf6f6
J-D-K <gerplefart@gmail.com>
2025-01-19 17:50:20 -0500

    Attempt to fix screw up

--- libs/FsLib/3DS/Examples/ExtData/include/IO.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "FsLib.hpp"
-
-void CopyDirectoryTo(const FsLib::Path &Source, const FsLib::Path &Destination);
--- libs/FsLib/Switch/FsLib/include/ErrorCommon.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern "C"
-{
-    static const char *ERROR_INVALID_PATH = "Invalid path supplied.";
-    static const char *ERROR_DEVICE_NOT_FOUND = "Device not found.";
-}
